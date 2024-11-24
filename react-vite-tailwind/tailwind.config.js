88eb60c94075142c5a12ce39f45134e23a0176cc
Sanjay Gawai <sanjaygawai1002@gmail.com>
2024-11-24 16:18:19 +0530

    added navbar

---
+++ react-vite-tailwind/tailwind.config.js
@@ -0,0 +1,14 @@
+const plugin = require('flowbite/plugin');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [plugin],
+};
